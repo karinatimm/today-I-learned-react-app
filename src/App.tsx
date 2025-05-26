7b2029d8854693454db2a83bff5e80e033197e04
karinatimm <kartimm.webdev@gmail.com>
2025-05-26 16:01:36 +0200

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
 import "./style.css";
 import Header from "./components/Header";
-import { useState } from "react";
-import FactForm from "./components/FactForm";
+import { useState, useEffect } from "react";
+import NewFactForm from "./components/NewFactForm";
+import FactList from "./components/FactList";
+import supabase from ".supabase";
 
 const App = () => {
   const [showForm, setShowForm] = useState<boolean>(false);
+  const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCategory, setCurrentCategory] = useState("all");
+
+  useEffect(() => {
+    const getFacts = async () => {
+      setIsLoading(true);
+
+      let query = supabase.from("facts").select("*");
+
+      if (currentCategory !== "all") {
+        query = query.eq("category", currentCategory);
+      }
+
+      const { data: facts, error } = await query
+        .order("votesInteresting", { ascending: false })
+        .limit(1000);
+
+      if (!error) {
+        setFacts(facts);
+      } else {
+        alert("There was a problem getting data");
+      }
+
+      setIsLoading(false);
+    };
+    getFacts();
+  }, [currentCategory]);
+
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm} />
-      <FactForm />
+      {showForm && (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      )}
+      <main className="main">
+        <CategoryFilter setCurrentCategory={setCurrentCategory} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <FactList facts={facts} setFacts={setFacts} />
+        )}
+      </main>
     </>
   );
 };
